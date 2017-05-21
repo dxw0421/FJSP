1461bd8e29905db4616701ac0d79f0c09f1069a8
junwen.ding <junwen.ding@NBN-03996>
2017-05-21 15:56:33 +0200

    The first commit.

---
+++ FJSP/fjsp.cpp
@@ -0,0 +1,18 @@
+#if 1
+#include<iostream>
+using namespace std;
+
+class FJSP
+{
+private:
+	int m, n;
+};
+int main(int argc, char **argv)
+{
+	cout << "This is the flexible job shop scheduling problem" << endl;
+#ifdef _WIN32
+	system("pause");
+#endif
+	return 0;
+}
+#endif
