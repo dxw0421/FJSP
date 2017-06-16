0176a63c42793fdfaa6a22c966141bbba76a0630
Jerry Ding <Jerry Ding@DESKTOP-JERRY>
2017-06-16 11:45:43 +0800

    The critical path and critical operations have been determined.

--- FJSP/fjsp.cpp
+++ FJSP/fjsp.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<stack>
 #include<map>
 #include<string>
 using namespace std;
@@ -63,7 +64,7 @@ public:
 	class Operation
 	{
 	public:
-		int proc_i, mach, t, start_time, end_time, r, q;
+		int proc_i, mach, t, start_time, end_time, r, q;	// optimize by removing proc_i or mach and t
 		Operation *pre_mach_oper, *next_mach_oper;
 		const int job_i, oper_i;	// the job and operation index of this operation
 		int in_degree;
@@ -83,7 +84,7 @@ public:
 Solution::Solution(int _m, int _mm) :makespan(_m), makespan_mach(_mm), start_dummy_oper(NULL), end_dummy_oper(NULL)
 {
 	start_dummy_oper = new Solution::Operation(0, 0, 0);
-	end_dummy_oper = new Solution::Operation(0, 0, 0);
+	end_dummy_oper = new Solution::Operation(0, 1000, 0);
 	start_dummy_oper->r = end_dummy_oper->q = 0;
 	start_dummy_oper->in_degree = end_dummy_oper->in_degree = 0;
 }
@@ -105,7 +106,7 @@ Solver::Solver(const Instance &_instance,int _sol_num) :instance(&_instance) ,so
 	{
 		Solution *sol = new Solution(0, 0);
 		sol->head_oper_mach_vec.resize(instance->m + 1, NULL);	// the initial value is NULL
-		sol->tail_oper_mach_vec.resize(instance->m + 1, NULL);	// the initial value is NULL
+		sol->tail_oper_mach_vec.resize(instance->m + 1, sol->start_dummy_oper);	// the initial value is start dummy operation
 		sol->job_vec.push_back(NULL);	// the first element in job_vec is NULL
 		for (int j = 1; j < instance->job_vec.size(); j++)
 		{
@@ -243,7 +244,7 @@ void Solver::check_solution(int sol_index)const
 void Solver::init_solution(int sol_index)
 {
 	vector<int> mach_ct_vec(instance->job_vec.size(), 0);	// the completion time of each machine
-	vector<Solution::Operation*> last_oper_mach(instance->m + 1,sol_vec[sol_index]->start_dummy_oper);	// the last operation at each machine
+	//vector<Solution::Operation*> last_oper_mach(instance->m + 1,sol_vec[sol_index]->start_dummy_oper);	// the last operation at each machine
 	vector<int> oper_to_assign_job(instance->job_vec.size(),1);	// the operation index needed to assign at each job
 	bool is_not_finished = true;
 	while (is_not_finished)
@@ -282,19 +283,19 @@ void Solver::init_solution(int sol_index)
 			sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->start_time = MAX(mach_ct_vec[min_ct_mach],
 				sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i] - 1]->end_time); 
 			sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->end_time = min_ct;
-			sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->pre_mach_oper = last_oper_mach[min_ct_mach];
+			sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->pre_mach_oper = sol_vec[sol_index]->tail_oper_mach_vec[min_ct_mach];
 			sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->next_mach_oper = sol_vec[sol_index]->end_dummy_oper;	// the next operation with the same machine is dummy end operation
 			mach_ct_vec[min_ct_mach] = min_ct;
-			sol_vec[sol_index]->tail_oper_mach_vec[min_ct_mach] = sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]];	// the tail operation assigned to the machine
+			//sol_vec[sol_index]->tail_oper_mach_vec[min_ct_mach] = sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]];	// the tail operation assigned to the machine
 			if (sol_vec[sol_index]->head_oper_mach_vec[min_ct_mach] == NULL)	// assign the first operation to the machine
 			{
 				sol_vec[sol_index]->head_oper_mach_vec[min_ct_mach] = sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]];
 				sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->in_degree -= 1;	// the degree of the first opertion at the machine is 1
 			}
-			if (last_oper_mach[min_ct_mach]->job_i != 0)	// this is not the first operation of the machine, assgin the next operation of the previous operation
+			if (sol_vec[sol_index]->tail_oper_mach_vec[min_ct_mach]->job_i != 0)	// this is not the first operation of the machine, assgin the next operation of the previous operation
 				sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]]->pre_mach_oper->next_mach_oper =
 				sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]];
-			last_oper_mach[min_ct_mach] = sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]];	// update the last operation at this machine
+			sol_vec[sol_index]->tail_oper_mach_vec[min_ct_mach]= sol_vec[sol_index]->job_vec[job_i]->oper_vec[oper_to_assign_job[job_i]];	// update the tail operation at this machine
 			oper_to_assign_job[job_i] += 1;	// the next operation is to be assigned
 			if (oper_to_assign_job[job_i] >= instance->job_vec[job_i]->oper_vec.size())
 				oper_to_assign_job[job_i] = 0;	// All the operations of this job is completed
@@ -312,7 +313,9 @@ void Solver::init_solution(int sol_index)
 void Solver::determine_critical_path(int sol_index)
 {
 	cout << "***solution " << sol_index << ", critical path***" << endl;
-	vector<Solution::Operation *> oper_in_degree_vec;
+	vector<Solution::Operation *> oper_in_degree_vec;	// store operations with in-degree of 0
+	stack<Solution::Operation*> oper_re_top_order_stack;	// store operations in stack to get re-topological order
+	stack<Solution::Operation*> critical_oper_stack;	// store critical operations
 	for (int mach_i = 1; mach_i < sol_vec[sol_index]->head_oper_mach_vec.size(); mach_i++)
 	{
 		Solution::Operation *oper = sol_vec[sol_index]->head_oper_mach_vec[mach_i];
@@ -337,7 +340,26 @@ void Solver::determine_critical_path(int sol_index)
 		if (sol_vec[sol_index]->job_vec[oper->job_i]->oper_vec[oper->oper_i + 1]->in_degree == 0 &&
 			oper->next_mach_oper != sol_vec[sol_index]->job_vec[oper->job_i]->oper_vec[oper->oper_i + 1])	// avoid the next machine operation is the same as the next job operation
 			oper_in_degree_vec.push_back(sol_vec[sol_index]->job_vec[oper->job_i]->oper_vec[oper->oper_i + 1]);
+		oper_re_top_order_stack.push(oper_in_degree_vec.front());	// push the first element into stack
 		oper_in_degree_vec.erase(oper_in_degree_vec.begin());	// delete the first element
+	}
+	cout << endl;
+	while (!oper_re_top_order_stack.empty())
+	{
+		Solution::Operation *oper = oper_re_top_order_stack.top();
+		oper->q = MAX(oper->next_mach_oper->q + oper->next_mach_oper->t,
+			sol_vec[sol_index]->job_vec[oper->job_i]->oper_vec[oper->oper_i + 1]->q + sol_vec[sol_index]->job_vec[oper->job_i]->oper_vec[oper->oper_i + 1]->t);
+		oper_re_top_order_stack.pop();
+		if (oper->r + oper->q + oper->t == sol_vec[sol_index]->makespan)
+			critical_oper_stack.push(oper);
+	}
+	cout << "critical operations: ";
+	while (!critical_oper_stack.empty())
+	{
+		Solution::Operation *oper = critical_oper_stack.top();
+		cout << oper->job_i << ", " << oper->oper_i << "\t" << oper->mach << "\t"
+			<< oper->start_time << "\t" << oper->end_time << "\t";
+		critical_oper_stack.pop();
 	}
 	if (num_oper_visited != instance->total_num_operation)
 		cout << "ERROR: not all operations are visited" << endl;
@@ -406,7 +428,7 @@ int main(int argc, char **argv)
 {
 	char *argv_win[] = { "",	// 0
 		"_ifp", "instances\\Dauzere_Data\\",
-		"_ifn", "02a",
+		"_ifn", "01a",
 		"_sol_num", "5"
 	};
 	cout << "This is the flexible job shop scheduling problem" << endl;
